99aa86a1385a7585094c5d709b8c833008bc5edd
Andres A. Acevedo <andy_xz@live.com>
2016-09-26 19:17:24 -0400

    First Commit
    Added all source files

---
+++ Game/Logic.java
@@ -0,0 +1,34 @@
+package Game;
+
+public class Logic {
+
+	public static String commands(String text, String user){
+
+		if (text.equalsIgnoreCase("Fire"))
+			return "\n   _O/\n     \\ \n     /\\_  \n     \\  ` \n     `   ";
+		return "zero";
+
+	}
+
+	public static int damage(int command){
+		int calc = 0;
+		
+		
+		return calc;
+	}
+	
+	public static void setCommand(String message){
+		
+		
+	}
+
+//	public static void decisions (){
+//		
+//	}
+//	
+//	public static String characters(int numOfPlayers, ){
+//		
+//	}
+//	
+	
+}
---
+++ Server/EchoClient.java
@@ -0,0 +1,42 @@
+package Server;
+
+import java.io.*;
+import java.net.*;
+
+public class EchoClient {
+	public static void main(String[] args) throws IOException {
+		String hostName = "ANDY-PC";
+		int port = 6980;
+		String username;
+		BufferedReader user =	new BufferedReader( new InputStreamReader(System.in));
+		
+		System.out.print("Enter username: ");
+		username = user.readLine();
+		
+		//begin connection
+		try (
+				Socket echoSocket = new Socket("ANDY-PC", port);
+				PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+				BufferedReader in =	new BufferedReader( new InputStreamReader(echoSocket.getInputStream()));
+				BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
+				) {
+			String userInput;
+			System.out.println("You are now in the chat!\n");
+			
+			while ((userInput = stdIn.readLine()) != null) {
+				out.println(userInput);
+				System.out.println(username +": " + in.readLine());
+				System.out.println(Game.Logic.commands("Fire", username));
+			}
+			
+			//error catching
+		} catch (UnknownHostException e) {
+			System.err.println("Don't know about host " + hostName);
+			System.exit(1);
+		} catch (IOException e) {
+			System.err.println("Couldn't get I/O for the connection to " +
+					hostName);
+			System.exit(1);
+		} 
+	}
+}
